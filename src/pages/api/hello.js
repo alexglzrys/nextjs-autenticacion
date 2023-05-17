f5b0983f07c5fc2b5c692a74cc09722868d703d6
Alejandro González Reyes <alexglzrys@gmail.com>
2023-05-16 18:46:23 -0600

    Rest API

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
+import { getSession } from "next-auth/react"
+
+export default async function handler(req, res) {
+  // Next Auth nos permite obtener la session tanto en cliente como en el servidor
+  // En el lado del servidor (Rest API) se le pasa el request como parámetro
+  // En getServerSideProps se le pasa el context
+  const session = await getSession({ req });
+  
+  if (!session) return res.status(403).json({message: 'Acceso no autorizado'});
+  
   res.status(200).json({ name: 'John Doe' })
 }
