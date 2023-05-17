0ff825f49bc13cf6c289845673f6f7793634160a
Alejandro González Reyes <alexglzrys@gmail.com>
2023-05-16 19:16:30 -0600

    Adaptador para MongoDB

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -6,8 +6,10 @@
  * incolucradas en el proceso de autenticación
  */
 
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 import NextAuth from "next-auth/next"
 import GithubProvider from 'next-auth/providers/github'
+import clientPromise from '@/lib/mongoClient'
 
 // Configuración de proveedores y rutas dinámicas de autenticación
 export const authOptions = {
@@ -18,7 +20,10 @@ export const authOptions = {
             clientId: process.env.GITHUB_CLIENT_ID,
             clientSecret: process.env.GITHUB_CLIENT_SECRET
         })
-    ]
+    ],
+    // Los adaptadores en Next Auth nos permiten guardar la info publica de la sessión en base de datos
+    // Entonces la info se tiene tanto en la Cookie de sessión como en la BD
+    adapter: MongoDBAdapter(clientPromise),
 }
 
 export default NextAuth(authOptions);
